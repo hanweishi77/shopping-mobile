1740c37a37b0bf6ff7173e2f36724e40e11a2347
hanweishi77 <15735181353@163.com>
2023-11-29 09:46:54 +0800

    login静态页面完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/utils/vant-ui'
+import '@/styles/common.css'
 
 Vue.config.productionTip = false
 
--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import { Button, Icon, Tabbar, TabbarItem } from 'vant'
+import { Button, Icon, Tabbar, TabbarItem, NavBar } from 'vant'
 
 Vue.use(Button)
 Vue.use(Icon)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(NavBar)
